5b771316480e2b07dada8ffe7bf6322c41e86b53
we60wl <naxi03@mail.ru>
2025-11-06 00:12:31 +0300

    add hello route

--- routes/root.js
+++ routes/root.js
@@ -2,4 +2,67 @@ export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+
+  fastify.get('/welcome', (req, res) => {
+    res.type('text/html')
+    res.send('<h1>Hello Hexlet</h1>')
+  })
+
+  fastify.get('/custom-header', (req, res) => {
+    res.header('key', 'value')
+    res.code(403)
+    res.send()
+  })
+
+  fastify.get('/user-json', (req, res) => {
+    // res.type('text/html')
+    res.send({ userName: 'user1' })
+  })
+
+  fastify.get('/admin', (req, reply) => {
+    reply.redirect('/', 302)
+  })
+
+  fastify.get('/users', (req, res) => {
+    const page = req.query.page
+    res.send(req.originalUrl)
+  })
+
+  fastify.post('/users', (req, res) => {
+    const page = req.query.page
+      res.send('POST /users')
+  })
+
+  fastify.get('/params', (req, res) => {
+    res.send(req.body) // Тело запроса
+    res.send(req.headers) // Заголовоки
+
+    res.send(req.query) // Все параметры запросов
+    res.send(req.query).name // Параметр запроса name
+  })
+
+  const state = {
+    users: [
+      {
+        id: 1,
+        name: 'user',
+      },
+    ],
+  }
+
+  fastify.get('/search', (req, res) => {
+    const { id } = req.query
+    const user = state.users.find(user => user.id === parseInt(id))
+    if (!user) {
+      res.code(404).send({ message: "User not found" })
+    }
+    else {
+      res.send(user)
+    }
+  })
+
+  fastify.get('/hello', (req, res) => {
+    const { name } = req.query
+    res.send(`Hello, ${name}!`)
+  })
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+    res.send('GET /users')
+})
+
+app.post('/users', (req, res) => {
+    res.send('POST /users')
+})
+
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
